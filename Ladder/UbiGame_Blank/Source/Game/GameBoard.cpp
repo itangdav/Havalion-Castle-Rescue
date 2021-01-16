98bd76a5ee388c0fa39000cb7a779fc0acfc896e
Jacky He <jacky.he.juncheng@gmail.com>
2021-01-15 23:02:12 -0500

    first commit

---
+++ Ladder/UbiGame_Blank/Source/Game/GameBoard.cpp
@@ -0,0 +1,27 @@
+#include "GameBoard.h"
+
+#include "GameEngine/GameEngineMain.h"
+
+
+using namespace Game;
+
+GameBoard::GameBoard()
+{
+    
+}
+
+
+GameBoard::~GameBoard()
+{
+    
+}
+
+void GameBoard::CreatePlayer()
+{
+    
+}
+
+void GameBoard::Update()
+{	
+	
+}
