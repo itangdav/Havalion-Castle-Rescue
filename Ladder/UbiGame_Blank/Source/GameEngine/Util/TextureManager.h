829a94e8f4abfbc611758220ac3095055448a332
alextxu <38475194+alextxu@users.noreply.github.com>
2021-01-16 16:17:39 -0500

    Added fog to top of screen.

--- Ladder/UbiGame_Blank/Source/GameEngine/Util/TextureManager.h
+++ Ladder/UbiGame_Blank/Source/GameEngine/Util/TextureManager.h
@@ -15,6 +15,7 @@ namespace GameEngine
 			Ladder,
 			Wall,
             Rock,
+			Fog,
 			Count,
 		};
 	}	
@@ -27,6 +28,7 @@ namespace GameEngine
 			case eTexture::Ladder: return "ladder-realistic.png";
 			case eTexture::Wall: return "brick-wall-v1.1.png";
             case eTexture::Rock: return "Rock.png";
+			case eTexture::Fog: return "fog-overlay.png";
             default: return "UnknownTextType";
         }
 		return "UnknownTexType";
